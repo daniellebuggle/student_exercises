fdae7df5ccaa2a09f7873cf95e91e20e68c6fdd4
Danielle <daniellebuggle7@gmail.com>
2024-11-08 17:34:14 +0000

    Add OR operator exercise

---
+++ or_operator.py
@@ -0,0 +1,13 @@
+"""
+Write a Python program that checks if a letter is a vowel ('a', 'e', 'i', 'o',
+or 'u'). Store your result in a variable called 'output'.
+"""
+
+letter = 'b'
+
+if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+    output = True
+else:
+    output = False
+
+print(output)
